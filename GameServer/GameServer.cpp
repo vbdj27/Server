c48854e489b1c4833787462513d8f6739cb13148
민규 김 <vbdj27@naver.com>
2023-08-30 22:02:49 +0900

    SpinLock

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,44 +4,77 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
-#include "AccountManager.h"
-#include "UserManager.h"
 
-void Func1()
+class SpinLock
 {
-	for (INT32 i = 0; i < 100; i++)
+public:
+	void lock()
 	{
-		UserManager::Instance()->ProcessSave();
+		// CAS (Compare-Add-Swap)
+		bool expected = false;
+		bool desired = true;
+
+		// CAS 의사 코드
+		/*if (_locked == expected)
+		{
+			expected = _locked;
+			_locked = desired;
+
+			return true;
+		}
+
+		else
+		{
+			expected = _locked;
+
+			return false;
+		}*/
+
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false;
+		}
+	}
+
+	void unlock()
+	{
+		_locked.store(false);
+	}
+
+private:
+	atomic<bool> _locked = false;
+};
+
+mutex m;
+INT32 sum = 0;
+SpinLock spinLock;
+
+void Add()
+{
+	for (INT32 i = 0; i < 100000; i++)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum++;
 	}
 }
 
-void Func2()
+void Sub()
 {
-	for (INT32 i = 0; i < 100; i++)
+	for (INT32 i = 0; i < 100000; i++)
 	{
-		AccountManager::Instance()->ProcessLogin();
+		lock_guard<SpinLock> guard(spinLock);
+		sum--;
 	}
 }
 
 int main()
 {
-	std::thread t1(Func1);
-	std::thread t2(Func2);
+	thread t1(Add);
+	thread t2(Sub);
 
 	t1.join();
 	t2.join();
 
-	cout << "Jobs Done" << endl;
-
-	// 락을 거는 순서를 동일하게 해서 데드락을 방지해야함
-
-	//참고
-	mutex m1;
-	mutex m2;
-	std::lock(m1, m2); // m1.lock(); m2.lock();
-
-	//adopt_lock : 이미 lock된 상태니까, 나중에 소멸될 때 락 해제
-	lock_guard<mutex> g1(m1, std::adopt_lock);
-	lock_guard<mutex> g1(m2, std::adopt_lock);
+	cout << sum << endl;
 }
 
