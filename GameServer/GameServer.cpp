64ef8ce1f207342da22af576fa7398986cf1eded
민규 김 <vbdj27@naver.com>
2023-09-08 16:13:57 +0900

    condition variable

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -10,17 +10,25 @@ mutex m;
 queue<int32> q;
 HANDLE handle;
 
+// 참고) CV는 User Level Object (커널 오브젝트 X)
+condition_variable cv;
+
 void Producer()
 {
 	while (true)
 	{
+		// 1) Lock을 잡고
+		// 2) 공유 변수 값을 수정
+		// 3) Lock을 풀고
+		// 4) 조건변수 통해 다른 스레드에게 통제
+
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 		
 
-		::SetEvent(handle); //시그널 변경
+		cv.notify_one(); // wait중인 스레드가 있으면 1개만 깨운다.
 
 		this_thread::sleep_for(100ms);
 
@@ -33,16 +41,24 @@ void Consumer()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE); // handle의 시그널 상태에 따라 동작변경
-		// ::ResetEvent(handle);
 
-		unique_lock<mutex> lock(m);
+		unique_lock<mutex> lock(m)
+		cv.wait(lock, []() {return q.empty() == false; });
 
-		if (q.empty() == false)
+		// 1) Lock을 잡고
+		// 2) 조건 확인
+		// 3) 만족 => 빠져나와서 다음코드 진행
+		//    불만족 => Lock을 풀어주고 대기 상태로 전환
+
+		// 그런데 notify_one을 했으면 항상 조건식을 만족하는거 아닐까?
+		// Spurious WakeUp (가짜 기상?)
+		// notify_one을 할 때 lock을 잡고 있는 것이 아니기 때문
+
+		//while (q.empty() == false)
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }
