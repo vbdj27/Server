c6ccacd4a3da9fa0b840bef2f8055fb2a9fefee7
민규 김 <vbdj27@naver.com>
2023-09-11 18:22:12 +0900

    Lock-Based Stack/Queue

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,33 +6,46 @@
 #include <mutex>
 #include <windows.h>
 #include <future>
+#include "ConcurrentQueue.h"
+#include "ConcurrentStack.h"
 
-thread_local int32 LThreadid = 0; // TLS 영역에 저장되는 변수
+LockQueue<int32> q;
+LockStack<int32> s;
 
-void ThreadMain(int32 threadid)
+void Push()
 {
-	LThreadid = threadid;
-
 	while (true)
 	{
-		cout << "Hi I am Thread " << LThreadid << endl;
-		this_thread::sleep_for(1s);
+		int32 value = rand() % 100;
+		q.Push(value);
+
+		this_thread::sleep_for(10ms);
 	}
 }
 
+void Pop()
+{
+	while (true)
+	{
+		int32 data = 0;
+
+		if (q.TryPop(OUT data))
+		{
+			cout << data << endl;
+		}
+		
+	}
+}
+
+
 int main()
 {
-	vector<thread> threads;
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
 
-	for (int32 i = 0; i < 10; i++)
-	{
-		int32 threadid = i + 1;
-		threads.push_back(thread(ThreadMain, threadid));
-	}
-
-	for (thread& t : threads)
-	{
-		t.join();
-	}
+	t1.join();
+	t2.join();
+	t3.join();
 }
 
