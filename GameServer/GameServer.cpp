10369ccc5507019979a99b38036730834dcf3fa4
민규 김 <vbdj27@naver.com>
2023-09-11 16:12:42 +0900

    cpu 파이프라인

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,42 +7,58 @@
 #include <windows.h>
 #include <future>
 
-int32 buffer[10000][10000];
+// 가시성, 코드 재배치
+// Atomic 연산 : 더 이상 쪼갤수 없어 한번에 연산이 처리됨(atomic 클래스 의존x)
+// atomic 연산에 한해, 모든 스레드가 동일 객체에 대해서 동일한 수정 순서를 관찰
+
+int32 x = 0;
+int32 y = 0;
+int32 r1 = 0;
+int32 r2 = 0;
+
+volatile bool ready;
+
+void Thread_1()
+{
+	while (!ready);
+
+	y = 1; // store y
+	r1 = x; // load x
+}
+
+void Thread_2()
+{
+	while (!ready);
+
+	x = 1; // store x
+	r2 = y; // load y
+}
 
 int main()
 {
-	memset(buffer, 0, sizeof(buffer));
+	int32 count = 0;
 
+	while (true)
 	{
-		uint64 start = GetTickCount64();
-		int64 sum = 0;
+		ready = false;
+		count++;
 
-		for (int32 i = 0; i < 10000; i++)
+		x = y = r1 = r2 = 0;
+
+		thread t1(Thread_1);
+		thread t2(Thread_2);
+
+		ready = true;
+
+		t1.join();
+		t2.join();
+
+		if (r1 == 0 && r2 == 0)
 		{
-			for (int32 j = 0; j < 10000; j++)
-			{
-				sum += buffer[i][j];
-			}
+			break;
 		}
-
-		uint64 end = GetTickCount64();
-		cout << "Elapsed Tick" << (end - start) << endl;
 	}
 
-	{
-		uint64 start = GetTickCount64();
-		int64 sum = 0;
-
-		for (int32 i = 0; i < 10000; i++)
-		{
-			for (int32 j = 0; j < 10000; j++)
-			{
-				sum += buffer[j][i];
-			}
-		}
-
-		uint64 end = GetTickCount64();
-		cout << "Elapsed Tick" << (end - start) << endl;
-	}
+	cout << count << " 번만에 빠져나옴" << endl;
 }
 
