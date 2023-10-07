7cd2eadc4ffebf31484c777066e8a6e967bdadcd
민규 김 <vbdj27@naver.com>
2023-10-07 17:20:21 +0900

    ThreadManager

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,45 +6,19 @@
 #include <mutex>
 #include <windows.h>
 #include <future>
-#include "ConcurrentQueue.h"
-#include "ConcurrentStack.h"
 
-LockQueue<int32> q;
-LockFreeStack<int32> s;
+CoreGlobal Core;
 
-void Push()
+void ThreadMain()
 {
 	while (true)
 	{
-		int32 value = rand() % 100;
-		s.Push(value);
-
-		this_thread::sleep_for(10ms);
+		cout << "Hello ! I am thread...";
 	}
 }
-
-void Pop()
-{
-	while (true)
-	{
-		auto data = s.TryPop();
-
-		if (data != nullptr)
-		{
-			cout << (*data) << endl;
-		}
-	}
-}
-
 
 int main()
 {
-	thread t1(Push);
-	thread t2(Pop);
-	thread t3(Pop);
-
-	t1.join();
-	t2.join();
-	t3.join();
+	
 }
 
--- ServerCore/CorePch.h
+++ ServerCore/CorePch.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Types.h"
+#include "CoreGlobal.h"
+#include "CoreTLS.h"
 #include <vector>
 #include <list>
 #include <queue>
