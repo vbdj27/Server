22dd21af61203ebe953d0df57be55426afc7b188
민규 김 <vbdj27@naver.com>
2024-04-22 17:12:52 +0900

    Commit

---
+++ ServerCore/Session.h
@@ -0,0 +1,113 @@
+#pragma once
+#include "IocpCore.h"
+#include "IocpEvent.h"
+#include "NetAddress.h"
+#include "RecvBuffer.h"
+#include "Types.h"
+
+class Service;
+
+/*--------------
+	Session
+---------------*/
+
+class Session : public IocpObject
+{
+	friend class Listener;
+	friend class IocpCore;
+	friend class Service;
+
+	enum
+	{
+		BUFFER_SIZE = 0x10000, // 64KB
+	};
+
+public:
+	Session();
+	virtual ~Session();
+
+public:
+						/* ?????? ??? */
+	void				Send(SendBufferRef sendBuffer);
+	bool				Connect();
+	void				Disconnect(const WCHAR* cause);
+
+	shared_ptr<Service>	GetService() { return _service.lock(); }
+	void				SetService(shared_ptr<Service> service) { _service = service; }
+
+public:
+						/* ???? ???? */
+	void				SetNetAddress(NetAddress address) { _netAddress = address; }
+	NetAddress			GetAddress() { return _netAddress; }
+	SOCKET				GetSocket() { return _socket; }
+	bool				IsConnected() { return _connected; }
+	SessionRef			GetSessionRef() { return static_pointer_cast<Session>(shared_from_this()); }
+
+private:
+						/* ????????? ???? */
+	virtual HANDLE		GetHandle() override;
+	virtual void		Dispatch(class IocpEvent* iocpEvent, int32 numOfBytes = 0) override;
+
+private:
+						/* ???? ???? */
+	bool				RegisterConnect();
+	bool				RegisterDisconnect();
+	void				RegisterRecv();
+	void				RegisterSend();
+
+	void				ProcessConnect();
+	void				ProcessDisconnect();
+	void				ProcessRecv(int32 numOfBytes);
+	void				ProcessSend(int32 numOfBytes);
+
+	void				HandleError(int32 errorCode);
+
+protected:
+						/* ?????? ????? ?????? */
+	virtual void		OnConnected() { }
+	virtual int32		OnRecv(BYTE* buffer, int32 len) { return len; }
+	virtual void		OnSend(int32 len) { }
+	virtual void		OnDisconnected() { }
+
+private:
+	weak_ptr<Service>	_service;
+	SOCKET				_socket = INVALID_SOCKET;
+	NetAddress			_netAddress = {};
+	Atomic<bool>		_connected = false;
+
+private:
+	USE_LOCK;
+
+							/* 수신 관련 */
+	RecvBuffer				_recvBuffer;
+
+							/* 송신 관련 */
+	Queue<SendBufferRef>	_sendQueue;
+	Atomic<bool>			_sendRegistered = false;
+
+private:
+						/* IocpEvent ???? */
+	ConnectEvent		_connectEvent;
+	DisconnectEvent		_disconnectEvent;
+	RecvEvent			_recvEvent;
+	SendEvent			_sendEvent;
+};
+
+struct PacketHeader
+{
+	uint16 size;
+	uint16 id; // 프로토콜ID
+};
+
+class PacketSession : public Session
+{
+public:
+	PacketSession();
+	virtual ~PacketSession();
+
+	PacketSessionRef GetPacketSessionRef() { return static_pointer_cast<PacketSession>(shared_from_this()); }
+
+protected:
+	virtual int32 OnRecv(BYTE* buffer, int32 len) sealed;
+	virtual int32 OnRecvPacket(BYTE* buffer, int32 len) abstract;
+};
